c6cac9c4b1e25ec34e4520a5472af536877cba12
MauriJHN <maurijhn1@gmail.com>
2023-07-16 12:00:25 -0300

    Added logic to add categories and read them
    Still need to clean up logic inside main to decide which operation to do
    before killing the program

--- bread/add_categories.py
+++ bread/add_categories.py
@@ -5,10 +5,9 @@ import enum
 CATEGORY_MAPPING = "categories.json"
 
 
-class Operations(enum.Enum):
-    READ = "read"
+class ModifyOptions(enum.Enum):
+    READ_KEYWORDS = "read keywords"
     ADD_KEYWORD = "add keyword"
-    ADD = "add"
 
 
 def print_options(options: list) -> None:
@@ -75,34 +74,79 @@ def ask_operation() -> str:
     Return:
         enum describing operation
     """
-    operation_options = list(map(lambda x: x.value, Operations))
+    operation_options = list(map(lambda x: x.value, ModifyOptions))
     operation = get_option_input(options=operation_options)
 
     return operation
 
 
-def add_keyword(categories: dict, category: str, keyword: str):
-    """Adds a keyword to a specific category"""
+def add_keyword(categories: dict, category: str, keyword: str) -> None:
+    """Adds a keyword to a specific category
+    
+    Args:
+        categories: dictionary containing category data
+        category: category where keywords are going to be added
+        keyword: keyword to be added
+    """
     categories[category].append(keyword)
 
 
-def add_category(categories: dict, category: str):
-    """Adds a new category"""
+def add_category(categories: dict, category: str) -> None:
+    """Adds a new category
+    
+    Args:
+        categories: dictionary containing category data
+        category: category where keywords are going to be added
+    """
     categories[category] = []
 
 
-def main():
-    # read the contents of categories.json
-    # ask which category you want to work in or if you want to add a new category
-    # ask if you want to read the keywords under a category or to add new ones
-    # if a new category is created, ask if keywords should be inserted
+def yes_no_input(message: str) -> bool:
+    """Helper function to get a yes or no input
+    
+    Args:
+        message: the message displayed in the prompt
+    Returns:
+        True if 'yes' or False if 'no' input
+    """
+    accepted_answers = ["yes", "y", "no", "n"]
+    answer = input(message)
+
+    while answer.lower() not in accepted_answers:
+        print(f"Please enter one of the following: {', '.join(accepted_answers)}")
+        answer = input(message)
+
+    if answer.lower() in accepted_answers[:2]:
+        return True
+    
+    if answer.lower() in accepted_answers[2:]:
+        return False
+
+
+def main() -> None:
     categories = read_categories()
+    add_new_category = yes_no_input("Would you like to add a new category?: ")
+
+
+    # TODO: create a logic to decide whether to add new category and end program or 
+    #       add new keyword and end program
+    if add_new_category:
+        new_category = input("Please enter the new category: ")
+        add_category(categories, category=new_category)
+        add_new_keyword = yes_no_input(f"Would you like to input keywords for {new_category}?: ")
+        if add_new_keyword:
+            new_keyword = input(f"Enter a keyword for {new_category} category: ")
+            add_keyword(categories, category=new_category, keyword=new_keyword)
+
     category = get_option_input(categories, message="Select one of the categories: ")
     operation = ask_operation()
 
-    if operation == Operations.ADD_KEYWORD.value:
+    if operation == ModifyOptions.ADD_KEYWORD.value:
         keyword = input(f"Enter a keyword for {category} category: ")
         add_keyword(categories, category, keyword)
+
+    if operation == ModifyOptions.READ_KEYWORDS.value:
+        print_options(categories[category])
 
     write_categories(categories)
 
